68c367eedbf86743d921f464b773a32001a7e3d3
Temik-Karpov <83543428+Temik-Karpov@users.noreply.github.com>
2022-09-22 12:25:31 +0300

    Initial commit

---
+++ src/main/java/ru/karpov/NewsPublic/NewsPublicApplication.java
@@ -0,0 +1,13 @@
+package ru.karpov.NewsPublic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewsPublicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewsPublicApplication.class, args);
+	}
+
+}
