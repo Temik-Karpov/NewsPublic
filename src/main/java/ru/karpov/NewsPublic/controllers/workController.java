daf7a3c9ffa32ed3c1f6c33038737aabf01d1fdd
Temik-Karpov <83543428+Temik-Karpov@users.noreply.github.com>
2022-12-08 21:36:47 +0300

    Add addNews method and showing news in mainPage

--- src/main/java/ru/karpov/NewsPublic/controllers/workController.java
+++ src/main/java/ru/karpov/NewsPublic/controllers/workController.java
@@ -1,32 +1,49 @@
 package ru.karpov.NewsPublic.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import ru.karpov.NewsPublic.models.News;
-import ru.karpov.NewsPublic.repos.userRepo;
 import ru.karpov.NewsPublic.repos.newsRepo;
 import ru.karpov.NewsPublic.repos.subscribeRepo;
+import ru.karpov.NewsPublic.repos.userRepo;
+
+import java.time.Instant;
+import java.util.Date;
 
 @Controller
 public class workController {
     private userRepo userRepo;
     private newsRepo newsRepo;
-    private subscribeRepo subscribeRepo;
+    private subscribeRepo subscribeRepol;
 
     @Autowired
-    public void WorkController(final userRepo userRepo, final newsRepo newsRepo, final subscribeRepo subscribeRepo)
+    public workController(final userRepo userRepo, final newsRepo newsRepo, final subscribeRepo subscribeRepo)
     {
         this.newsRepo = newsRepo;
         this.userRepo = userRepo;
-        this.subscribeRepo = subscribeRepo;
+        this.subscribeRepol = subscribeRepo;
     }
 
     @PostMapping("/addNews")
-    public String addNews(@ModelAttribute("news") News news, Model model)
+    public String addNews(@RequestParam("Title") String title, @RequestParam("text") String text,
+                          @RequestParam("category") String category,
+                          Model model)
     {
+        final Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        final String id = authentication.getName();
+
+        News news = new News();
+        news.setName(title);
+        news.setCategory(category);
+        news.setText(text);
+        news.setDate(Date.from(Instant.now()));
+        news.setAuthorName(userRepo.findUserById(id).getName());
         newsRepo.save(news);
         return "mainPage";
     }
@@ -42,7 +59,6 @@ public class workController {
     @PostMapping("/editNews")
     public String editNews(@ModelAttribute("news") News news, Model model)
     {
-
         return "mainPage";
     }
 
